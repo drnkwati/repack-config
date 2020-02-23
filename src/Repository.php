51a46999a4da58dbaa727fba10c96a93dae7f53d
nkwati <drnkwati@yahoo.com>
2020-02-24 00:41:04 +0400

    first commit

---
+++ src/Repository.php
@@ -0,0 +1,250 @@
+<?php
+
+namespace Repack\Config;
+
+if (interface_exists('\Illuminate\Contracts\Config\Repository')) {
+    interface ConfigContract extends \Illuminate\Contracts\Config\Repository
+    {}
+} else {
+    interface ConfigContract
+    {}
+}
+
+class Repository implements \ArrayAccess, ConfigContract
+{
+    /**
+     * All of the configuration items.
+     *
+     * @var array
+     */
+    protected $items = array();
+
+    /**
+     * Create a new configuration repository.
+     *
+     * @param  array  $items
+     * @return void
+     */
+    public function __construct(array $items = array())
+    {
+        $this->items = $items;
+    }
+
+    /**
+     * Get all of the configuration items for the application.
+     *
+     * @return array
+     */
+    public function all()
+    {
+        return $this->items;
+    }
+
+    /**
+     * Determine if the given configuration value exists.
+     *
+     * @param  string $key
+     * @return bool
+     */
+    public function has($key)
+    {
+        return static::arrGet($this->items, $key) ? true : false;
+    }
+
+    /**
+     * Get the specified configuration value.
+     *
+     * @param  string  $key
+     * @param  mixed   $default
+     * @return mixed
+     */
+    public function get($key, $default = null)
+    {
+        if (is_array($key)) {
+            return $this->getMany($key);
+        }
+
+        return static::arrGet($this->items, $key, $default);
+    }
+
+    /**
+     * Get many configuration values.
+     *
+     * @param  array  $keys
+     * @return array
+     */
+    public function getMany($keys)
+    {
+        $config = array();
+
+        foreach ($keys as $key => $default) {
+            if (is_numeric($key)) {
+                list($key, $default) = array($default, null);
+            }
+
+            $config[$key] = static::arrGet($this->items, $key, $default);
+        }
+
+        return $config;
+    }
+
+    /**
+     * Set a given configuration value.
+     *
+     * @param  array|string $key
+     * @param  mixed        $value
+     * @return void
+     */
+    public function set($key, $value = null)
+    {
+        if (is_array($key)) {
+            foreach ($key as $innerKey => $innerValue) {
+                static::arrSet($this->items, $innerKey, $innerValue);
+            }
+        } else {
+            static::arrSet($this->items, $key, $value);
+        }
+    }
+
+    /**
+     * Prepend a value onto an array configuration value.
+     *
+     * @param  string $key
+     * @param  mixed  $value
+     * @return void
+     */
+    public function prepend($key, $value)
+    {
+        $array = $this->get($key);
+
+        array_unshift($array, $value);
+
+        $this->set($key, $array);
+    }
+
+    /**
+     * Push a value onto an array configuration value.
+     *
+     * @param  string $key
+     * @param  mixed  $value
+     * @return void
+     */
+    public function push($key, $value)
+    {
+        $array = $this->get($key);
+
+        $array[] = $value;
+
+        $this->set($key, $array);
+    }
+
+    /**
+     * Determine if the given configuration option exists.
+     *
+     * @param  string $key
+     * @return bool
+     */
+    public function offsetExists($key)
+    {
+        return $this->has($key);
+    }
+
+    /**
+     * Get a configuration option.
+     *
+     * @param  string  $key
+     * @return mixed
+     */
+    public function offsetGet($key)
+    {
+        return $this->get($key);
+    }
+
+    /**
+     * Set a configuration option.
+     *
+     * @param  string $key
+     * @param  mixed  $value
+     * @return void
+     */
+    public function offsetSet($key, $value)
+    {
+        $this->set($key, $value);
+    }
+
+    /**
+     * Unset a configuration option.
+     *
+     * @param  string $key
+     * @return void
+     */
+    public function offsetUnset($key)
+    {
+        $this->set($key, null);
+    }
+
+    /**
+     * Get an item from an array using "dot" notation.
+     *
+     * @param  array   $array
+     * @param  string  $key
+     * @param  mixed   $default
+     * @return mixed
+     */
+    public static function arrGet($array, $key, $default = null)
+    {
+        if (is_null($key)) {
+            return $array;
+        }
+
+        if (isset($array[$key])) {
+            return $array[$key];
+        }
+
+        foreach (explode('.', $key) as $segment) {
+            if (!is_array($array) || !array_key_exists($segment, $array)) {
+                return $default instanceof \Closure ? $default() : $default;
+            }
+
+            $array = $array[$segment];
+        }
+
+        return $array;
+    }
+
+    /**
+     * Set an array item to a given value using "dot" notation.
+     *
+     * If no key is given to the method, the entire array will be replaced.
+     *
+     * @param  array   $array
+     * @param  string  $key
+     * @param  mixed   $value
+     * @return array
+     */
+    public static function arrSet(&$array, $key, $value)
+    {
+        if (is_null($key)) {
+            return $array = $value;
+        }
+
+        $keys = explode('.', $key);
+
+        while (count($keys) > 1) {
+            $key = array_shift($keys);
+
+            // If the key doesn't exist at this depth, we will just create an empty array
+            // to hold the next value, allowing us to create the arrays to hold final
+            // values at the correct depth. Then we'll keep digging into the array.
+            if (!isset($array[$key]) || !is_array($array[$key])) {
+                $array[$key] = array();
+            }
+
+            $array = &$array[$key];
+        }
+
+        $array[array_shift($keys)] = $value;
+
+        return $array;
+    }
+}
